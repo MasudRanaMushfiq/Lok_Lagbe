2f7676e55a42a855a665550cd559a12ded494a66
Masud_Rana <masudranaorg71@gmail.com>
2025-09-26 23:54:12 +0600

    Icon Changed

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -6,13 +6,6 @@ import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyBfzm2PMxv6l9IQA2lqXuDag7EcJQDeARA",
-  // authDomain: "loklagbe-cfcfd.firebaseapp.com",
-  // projectId: "loklagbe-cfcfd",
-  // storageBucket: "loklagbe-cfcfd.appspot.com",
-  // messagingSenderId: "106935080483",
-  // appId: "1:106935080483:web:bb0d13ec04dd66b4bfc396",
-  // measurementId: "G-T8K4PK0V7V"
   apiKey: "AIzaSyDGwnEn-_hKiLDQIlddTTvI2Qe9OFq7Y4U",
   authDomain: "lok-lagbe-f77de.firebaseapp.com",
   projectId: "lok-lagbe-f77de",
